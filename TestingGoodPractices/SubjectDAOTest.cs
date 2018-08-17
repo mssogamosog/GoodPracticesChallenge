6fa815cc5e4047cf86bc7a346951bd23166fbafc
Michael Sogamoso <mssg_2211@hotmail.com>
2018-08-17 16:23:25 -0500

    Testin SubjectDAO

--- TestingGoodPractices/SubjectDAOTest.cs
+++ TestingGoodPractices/SubjectDAOTest.cs
@@ -28,7 +28,7 @@ namespace TestingGoodPractices
 			var actual = cls.List();
 			//then
 			Assert.IsNotNull(actual);
-			Assert.AreEqual(actual.Count,expected.Count);
+			Assert.AreEqual(actual.Count, expected.Count);
 			for (int item = 0; item < actual.Count; item++)
 			{
 				Assert.AreEqual(actual[item].Courses, expected[item].Courses);
@@ -39,17 +39,58 @@ namespace TestingGoodPractices
 
 		}
 
+		[TestMethod]
+		public void Create_Is_Successful()
+		{
+			//given
+			Subject subject = SampleSubjects()[0];
+			var mockSet = GeneralMock.GetQueryableMockDbSet(SampleSubjects());
+			var mockMessaging = new Mock<IMessaging>();
+			var mockContext = new Mock<IDataBaseContext>();
+			mockContext.Setup(c => c.Subjects).Returns(mockSet.Object);
+			var cls = new SubjectDAO(mockContext.Object, mockMessaging.Object);
+
+			//when
+			cls.Create(subject.Name,subject.Description);
+			var actual = cls.List();
+			//then
+			mockSet.Verify(m => m.Add(It.IsAny<Subject>()), Times.Once());
+			mockContext.Verify(m => m.SaveChanges(), Times.Once());
+
+		}
+		[TestMethod]
+		public void SubjectsByTeacher_Is_Correct()
+		{
+			var mockSet = GeneralMock.GetQueryableMockDbSet(SampleSubjects());
+			var mockMessaging = new Mock<IMessaging>();
+			var mockContext = new Mock<IDataBaseContext>();
+			mockContext.Setup(c => c.Subjects).Returns(mockSet.Object);
+		}
+
 		private List<Subject> SampleSubjects()
 		{
 			List<Subject> output = new List<Subject>
 			{
-				new Subject{Name ="Course1" , Description = "Description 1"},
-				new Subject{Name ="Course2" , Description = "Description 2"},
-				new Subject{Name ="Course3" , Description = "Description 3"},
-				new Subject{Name ="Course4" , Description = "Description 4"},
-				new Subject{Name ="Course5" , Description = "Description 5"},
-				new Subject{Name ="Course6" , Description = "Description 6"},
-				new Subject{Name ="Course7" , Description = "Description 7"},
+				new Subject{Name ="Course Test1" , Description = "Description 1", Id = 1,},
+				new Subject{Name ="Course2" , Description = "Description 2", Id = 2},
+				new Subject{Name ="Course3" , Description = "Description 3", Id = 3},
+				new Subject{Name ="Course4" , Description = "Description 4", Id = 4},
+				new Subject{Name ="Course5" , Description = "Description 5", Id = 5},
+				new Subject{Name ="Course6" , Description = "Description 6", Id = 6},
+				new Subject{Name ="Course7" , Description = "Description 7", Id = 7},
+			};
+			return output;
+		}
+
+		private List<Teacher> SampleTeachers()
+		{
+			List<Teacher> output = new List<Teacher>
+			{
+				new Teacher{Name ="Teacher Test1" , Id = 1,Subjects = null },
+				new Teacher{Name ="Teacher2" , Id = 2},
+				new Teacher{Name ="Teacher3" , Id = 3},
+				new Teacher{Name ="Teacher4" , Id = 4},
+
 			};
 			return output;
 		}
